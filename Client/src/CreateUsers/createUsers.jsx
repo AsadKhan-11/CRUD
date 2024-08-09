c256eebb1c83c27309b21c196835e3815124b91a
Asad Khan <mrasad10khan@gmail.com>
2024-08-09 10:46:19 +0500

    Post request

--- Client/src/CreateUsers/createUsers.jsx
+++ Client/src/CreateUsers/createUsers.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./createUsers.css";
+import axios from "axios";
+
 function createUsers() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+
+  const Submit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/createUsers", { name, email, age })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="createUser">
+    <form className="createUser" onSubmit={Submit}>
       <h1>Add New User</h1>
       <div className="user-info">
         <label htmlFor="">Name</label>
-        <input type="text" placeholder="Enter your name" required />
+        <input
+          type="text"
+          placeholder="Enter your name"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div className="user-info">
         <label htmlFor="">Email</label>
-        <input type="email" placeholder="Enter your email" required />
+        <input
+          type="email"
+          placeholder="Enter your email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="user-info">
         <label htmlFor="">Age</label>
@@ -20,10 +44,11 @@ function createUsers() {
           max="100"
           placeholder="Enter your age"
           required
+          onChange={(e) => setAge(e.target.value)}
         />
       </div>
       <button type="submit">Add User</button>
-    </div>
+    </form>
   );
 }
 
